0d330db0d8ed5f99a96ba99cb7f14a14085b1894
dcd <haveachillday@gmail.com>
2019-09-25 15:48:21 +1200

    tweaked box animations

---
+++ client/src/app/animations/movetocursorvertically.ts
@@ -0,0 +1,20 @@
+import { animation, animate, state, trigger, style, query, AnimationStyleMetadata, useAnimation } from '@angular/animations';
+
+const movetocursorvertically = animation(
+    animate(
+        "0s",
+        style({
+            transform: "{{ inputstyle }}"
+        })
+    )
+)
+
+export const resetposition = animation(
+    animate(
+        "0.8s ease-out",
+        style({
+            transform: "translate(0px, 0px)"
+        })
+    )
+)
+export default movetocursorvertically
