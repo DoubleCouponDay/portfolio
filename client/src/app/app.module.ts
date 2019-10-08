79bc78cfbc19448ae438c59fa38570bc649764a1
dcd <haveachillday@gmail.com>
2019-10-09 11:20:34 +1300

    Update app.module.ts

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,11 +6,8 @@ import { AppComponent } from './app.component';
 import { LoadingscreenComponent } from './loadingscreen/loadingscreen.component';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { vectorscomponent } from './vectors/vectors.component';
 import { PagesModule } from './pages/pages.module';
 import { backgroundcomponent } from './background/background.component';
-import { TabletComponent } from './vectors/tablets/tablet/tablet.component';
-import { BlocksComponent } from './vectors/blocks/blocks.component';
 import { VectorsModule } from './vectors/vectors.module';
 import { MatSnackBarModule } from '@angular/material';
 
