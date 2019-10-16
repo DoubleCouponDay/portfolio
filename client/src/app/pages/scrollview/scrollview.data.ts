c0570e43881f770cad3a700cd8249b00f13ef854
dcd <haveachillday@gmail.com>
2019-10-17 08:10:42 +1100

    Revert "Revert "fix: audio is cached on page load""
    This reverts commit 7dad4b61d53f4d93eef4de2cdcef134d83a892af.

--- client/src/app/pages/scrollview/scrollview.data.ts
+++ client/src/app/pages/scrollview/scrollview.data.ts
@@ -4,7 +4,7 @@ export const maximumtranslation = 625
 export const scrollmultiplier = 2.6
 export const mintranslationX = 2
 export const mintranslationY = 1
-export const scrapesoundpath = 'assets/tabletscraping.mp3'
+
 export const nomovementtimer = 1000
 export const buttonidentifier = 'scrollbutton'
 export const millisecondspoint = 1000
