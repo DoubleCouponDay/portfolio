422956e271f11f7b6cf61a4746fb046d3e7db958
dcd <haveachillday@gmail.com>
2019-10-17 14:18:35 +1100

    added reverb to drawbridge

--- client/src/app/audio/audio.data.ts
+++ client/src/app/audio/audio.data.ts
@@ -5,5 +5,5 @@ export const volumeincrement = volumedecrement * 2
 export const maxvolume  = 1
 
 export const gratingsoundaddress = 'assets/stone-grinding.mp3'
-export const drawbridgesoundaddress = 'assets/drawbridge.mp3'
+export const drawbridgesoundaddress = 'assets/drawbridge.flac'
 export const tabletsoundaddress = 'assets/tabletscraping.mp3'
