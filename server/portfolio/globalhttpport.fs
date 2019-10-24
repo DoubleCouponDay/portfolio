d13c4472852eb9902f2a6ad7369a364fefb29328
dcd <haveachillday@gmail.com>
2019-10-24 14:39:19 +1100

    Draft: random desert track

---
+++ server/portfolio/globalhttpport.fs
@@ -0,0 +1,12 @@
+﻿module portfolio.globalhttpport
+
+open System.Net.Http
+open System
+
+type public globalhttpport =
+    static member get = new globalhttpport()
+
+    member public x.port = new HttpClient()
+
+    private new() = new globalhttpport()
+        
--- server/portfolio/googledrivereader.fs
+++ server/portfolio/googledrivereader.fs
@@ -11,6 +11,9 @@ open System.Security.Cryptography.X509Certificates
 open Newtonsoft.Json
 open FSharp.Data
 open portfolio.googleresponses
+open portfolio.globalhttpport
+open System.Linq
+open System.Collections.Generic
 
 [<Literal>]
 let credentialspath = @"portfolio-256800-2c7593f8c9a5.p12"
@@ -24,39 +27,67 @@ let serviceaccountemail = "server@portfolio-256800.iam.gserviceaccount.com"
 [<Literal>]
 let secretspath = "secrets.json"
 
+[<Literal>]
+let backslash = @"\"
+
 type secrets = JsonProvider<secretspath>
 
 let reader = secrets.Load(secretspath)
 
-type public drivereader() =
-    static member val public get = new drivereader()
-        with get
+let Scopes = [| DriveService.Scope.DriveReadonly |] 
 
-    member private x.Scopes = [| DriveService.Scope.DriveReadonly |] 
-    member private x.ApplicationName = "googledrivereader"
+let ApplicationName = "googledrivereader"
 
-    member public x.readrandomdeserttrack(): unit =
-        let privatekey: string = reader.Driveprivatekey
-        let certificate = new X509Certificate2(credentialspath, privatekey, X509KeyStorageFlags.Exportable);
+type public drivereader private() =    
+    let privatekey: string = reader.Driveprivatekey
+    let certificate = new X509Certificate2(credentialspath, privatekey, X509KeyStorageFlags.Exportable);
+    let mutable initializer = new ServiceAccountCredential.Initializer(serviceaccountemail)        
 
-        let mutable initializer = new ServiceAccountCredential.Initializer(serviceaccountemail)        
-        initializer.Scopes <- x.Scopes
+    do
+        initializer.Scopes <- Scopes
         initializer.User <- serviceaccountemail
         initializer <- initializer.FromCertificate(certificate)
-        let credential = new ServiceAccountCredential(initializer)
 
-        let baseservice = new BaseClientService.Initializer()
+    let credential = new ServiceAccountCredential(initializer)
+
+    let baseservice = new BaseClientService.Initializer()
+
+    do
         baseservice.HttpClientInitializer <- credential
-        baseservice.ApplicationName <- x.ApplicationName
-        let googledrive = new DriveService(baseservice)
+        baseservice.ApplicationName <- ApplicationName        
+    
+    let servicefield = new DriveService(baseservice)
 
+    static member public get = new drivereader()
+    member private x.service: DriveService = servicefield
+
+    member val private playlist: string[] = null
+        with get, set
+
+    member val private rng = new Random()
+
+    member public x.readrandomdeserttrack(): Data.File =    
+        if x.playlist = null then
+            x.setplaylist()
+
+        let chosenindex = x.rng.Next(x.playlist.Length)
+        let chosenfilename = x.playlist.[chosenindex]
+        x.requestfilebyname(chosenfilename)
+
+    member private x.setplaylist(): unit =
+        let playlistfile = x.requestfilebyname(playlistname)
+        x.playlist = x.getplaylistlines(playlistfile)
+        |> ignore
+        ()
+        
+    member private x.requestfilebyname(filename: string): Data.File =
         let verytrue = new Nullable<bool>(true)
-        let listRequest = googledrive.Files.List()            
+        let listRequest = x.service.Files.List()            
         listRequest.PageSize <- new Nullable<int>(1)
         listRequest.Fields <- "files(*)"
         listRequest.Spaces <- "drive"
         listRequest.Corpora <- "allDrives"
-        listRequest.Q <- "name = '" + playlistname + "'"
+        listRequest.Q <- "name = '" + filename + "'"
         listRequest.SupportsAllDrives <- verytrue
         listRequest.IncludeItemsFromAllDrives <- verytrue   
         
@@ -69,17 +100,21 @@ type public drivereader() =
             Console.WriteLine(message)
             failwith message
 
-        for file in files do
-            Console.Write(file.Name)
-            Console.Write("")
-            Console.Write("")
-            Console.Write("")
-        
+        files.Item(0)
+
+    member private x.getplaylistlines(playlist: Data.File): string[] =
+        let downloadlink = playlist.WebContentLink
+        use response = globalhttpport.get.port.GetAsync(downloadlink).Result        
+        use reader = new StreamReader(response.Content.ReadAsStreamAsync().Result)
+        let entire = reader.ReadToEnd()
+        entire.Split(Environment.NewLine)
+            .Select(fun currentline -> 
+                currentline.Split(backslash)
+                    .Last()
+            )
+            .ToArray()
+
+
         
 
-    member private x.findplaylistfile() =
-        ()
-
-    member private x.takerandomlinefromplaylist() =
-        ()
            
