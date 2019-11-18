8fe29cff95ddde54ca7adae901b4cdd31be8f7b4
dcd <haveachillday@gmail.com>
2019-11-18 13:31:34 +1300

    fix: handled infinite serialization loops

--- client/src/environments/api.ts
+++ client/src/environments/api.ts
@@ -1,6 +1,7 @@
 import { portfolioapi, randomdeserttrackroute } from './environment.data';
 
-export const baseroute = "http://localhost:60257"
+// export const baseroute = "http://localhost:60257"
+export const baseroute = "https://samueljenks.azurewebsites.net"
 
 export const api: portfolioapi = {
     getrandomtrack: `${randomdeserttrackroute}`
