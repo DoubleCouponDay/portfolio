73fb29503af7732a2150117f713ecfb8036d94e9
dcd <haveachillday@gmail.com>
2019-12-06 18:00:36 +1300

    fix: "length" needed to be greater than byte length / blockalign

--- server/portfolio/flacutils.fs
+++ server/portfolio/flacutils.fs
@@ -8,8 +8,6 @@ open System
 type public flacutils =
     static member public writeflacchunk(reader:FlakeReader, output: FlakeWriter, startposition: int64, endposition: int64): int =
         let takecount = int(endposition - startposition)
-        if takecount % reader.PCM.BlockAlign <> 0 then
-            failwith ("amount: " + string(takecount) + " to take must be a multiple of block align: " + string(reader.PCM.BlockAlign))
 
         let portion = 
             reader.Samples
@@ -18,7 +16,9 @@ type public flacutils =
                 .Select(fun item -> byte(item))
                 .ToArray()
 
-        let buffer = new AudioBuffer(reader.PCM, portion, portion.Length)
+        let length = portion.Length / reader.PCM.BlockAlign
+
+        let buffer = new AudioBuffer(reader.PCM, portion, length)
         output.WriteHeader()
         output.Write(buffer)
         
