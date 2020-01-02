0cb11d377ee2f07177478b395f47329c820b8652
dcd <haveachillday@gmail.com>
2020-01-02 19:49:01 +1300

    server: stream still had a little to clean up

--- server/portfolio/Controllers/audio/streamhub.fs
+++ server/portfolio/Controllers/audio/streamhub.fs
@@ -35,6 +35,7 @@ type streamhub() =
 
         if usedmemory >= availablememory then
             let message = "hit simultaneous stream count: " + string(streamhub.clientcount)
+            this.cleanup()
             raise (InsufficientMemoryException(message))
 
         this.fillchannel(channel) |> ignore
